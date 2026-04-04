5cc3bcd94a2378acb9721112c2079c5f26170609
sunnydasatan <sunnydasatan@gmail.com>
2026-04-04 05:39:31 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,11 @@
+SYMBOLS = {
+    "silver": "SI=F",
+    "gold": "GC=F",
+    "oil": "CL=F",
+    "dxy": "DX-Y.NYB",
+    "usdinr": "USDINR=X"
+}
+
+LOOKBACK = "2y"
+FORECAST_HORIZON = 5
+TARGET_VOL = 0.18
